620c1cadbea610a3d98832a62aa1e1b9fbac6642
Kantithat Kanfung <51295343+Kantithat-Kanfung@users.noreply.github.com>
2020-06-06 19:28:56 +0700

    Add files via upload

---
+++ Module/Md.vb
@@ -0,0 +1,89 @@
+﻿Imports System.Text
+Imports System.Data
+Imports System.Data.SqlClient
+Imports System.Security.Cryptography
+
+Module Md
+
+    ' Your Servername and Database
+    Public ConStr As String = My.Settings.ConStr
+
+    ' Test connect database
+    Public Sub ConnectDatabase()
+        Try
+            Dim Connection As New SqlConnection(ConStr)
+
+            If Connection.State = ConnectionState.Closed Then
+                Connection.Open()
+            End If
+
+            Connection.Close()
+        Catch ex As SqlException
+            WriteLogErrorConnectDatabase(ex.Message.ToString())
+        End Try
+    End Sub
+
+    ' Write Log file when can't disconnect database
+    Private Sub WriteLogErrorConnectDatabase(ByVal Content As String)
+        Dim fs = My.Computer.FileSystem
+
+        Dim f As String = "Logs\Log_Error_Connect_Database.Log"
+
+        If fs.GetFileInfo(f).Exists Then
+            fs.WriteAllText(f, Now.ToString("dd/MM/yyyy HH:mm:ss") & " || " & Content & vbNewLine, True)
+        Else
+            fs.CreateDirectory("Logs")
+            fs.WriteAllText(f, Now.ToString("dd/MM/yyyy HH:mm:ss") & " || " & Content & vbNewLine, True)
+        End If
+    End Sub
+
+    Public Sub WriteLogMistakeQuery(ByVal Content As String)
+        Dim fs = My.Computer.FileSystem
+
+        Dim f As String = "Logs\Log_Mistake_Query.Log"
+
+        If fs.GetFileInfo(f).Exists Then
+            fs.WriteAllText(f, Now.ToString("dd/MM/yyyy HH:mm:ss") & " || " & Content & vbNewLine, True)
+        Else
+            fs.CreateDirectory("Logs")
+            fs.WriteAllText(f, Now.ToString("dd/MM/yyyy HH:mm:ss") & " || " & Content & vbNewLine, True)
+        End If
+    End Sub
+
+    Public Sub WriteLogMistakeSendMail(ByVal Content As String)
+        Dim fs = My.Computer.FileSystem
+
+        Dim f As String = "Logs\Log_Mistake_Send_Mail.Log"
+
+        If fs.GetFileInfo(f).Exists Then
+            fs.WriteAllText(f, Now.ToString("dd/MM/yyyy HH:mm:ss") & " || " & Content & vbNewLine, True)
+        Else
+            fs.CreateDirectory("Logs")
+            fs.WriteAllText(f, Now.ToString("dd/MM/yyyy HH:mm:ss") & " || " & Content & vbNewLine, True)
+        End If
+    End Sub
+
+    Public Function MD5(ByVal text As String) As String
+        Dim Hash As New Security.Cryptography.MD5CryptoServiceProvider
+
+        Dim ByteString() As Byte = System.Text.Encoding.ASCII.GetBytes(text)
+
+        ByteString = Hash.ComputeHash(ByteString)
+
+        Dim FinalString As String = Nothing
+
+        For Each bt As Byte In ByteString
+            FinalString &= bt.ToString("x2")
+        Next
+
+        Return FinalString
+    End Function
+
+    ' About Application
+    Public ProductName As String = My.Settings.ProductName
+    Public Version As String = My.Settings.Version
+    Public Copyright As String = My.Settings.Copyright
+    Public Company As String = My.Settings.Company
+    Public Description As String = My.Settings.Description
+
+End Module
---
+++ My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("FrameMainScreen")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("FrameMainScreen")>
+<Assembly: AssemblyCopyright("Copyright ©  2020")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("0d21c5c7-7e6c-4484-b1b6-f11bf6075aa5")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
